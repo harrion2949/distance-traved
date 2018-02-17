3aea8154e852cd834dfa523e59f70ea16b25c82f
harrion2949 <36568433+harrion2949@users.noreply.github.com>
2018-02-17 10:47:20 -0500

    Add files via upload

---
+++ p2hw1_distancetraveled_harrison_terry.py
@@ -0,0 +1,9 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print('distance_after 6 is', 70*6)
+distance_after 6 is 420
+>>> print('distance_after 10 is', 70*10)
+distance_after 10 is 700
+>>> print('distance_after 15 is', 70*15)
+distance_after 15 is 1050
+>>> 
